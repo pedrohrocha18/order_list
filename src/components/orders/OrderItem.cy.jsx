4d514f5a6fe1f49ec4fd8ae01a5de9aaf07dc362
pedrohrocha18 <phr.tech@outlook.com>
2024-07-10 20:22:52 -0300

    Finalizando projeto

--- src/components/orders/OrderItem.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import OrderItem from './OrderItem'
-
-describe('<OrderItem />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<OrderItem />)
-  })
-})
